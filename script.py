0a2246cf789d5506688d9ed3cd3f79aebc98e56a
Vince Szabó <szabo.vince@icloud.com>
2025-03-17 10:00:30 +0100

    masonry layout

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
 i = 1
 
 while i < 33:
-    text = f'<img src="/thumbnails/{i}.jpg" class="hover thumbnail" onclick="openPopup({i})">'
+    # text = f'<img src="/thumbnails/{i}.jpg" class="hover thumbnail" onclick="openPopup({i})">'
+    text = f'''
+<div class="mItem">
+    <img src="/thumbnails/{i}.jpg" />
+</div>">
+'''
     print(text)
     i += 1
